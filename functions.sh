ab08b300a5ebd9f57e70aec7d29f84841611859d
andye <andye@yambay.com>
2019-05-14 17:31:35 +0800

    Updated echo with filenames

--- functions.sh
+++ functions.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
+echo "[functions.sh - sourcing] JAVA_HOME=${JAVA_HOME}"
+
 function build () {
-    echo "[functions.sh] JAVA_HOME=${JAVA_HOME}"
+    echo "[functions.sh - build called] JAVA_HOME=${JAVA_HOME}"
     ./build.sh
 }
